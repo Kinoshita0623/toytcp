6bc5be0774ad6ea65ea9a29f730926b14591a961
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-09-30 05:35:40 +0000

    [change] sendparam, recvparamを追加しました

--- src/socket.rs
+++ src/socket.rs
@@ -35,6 +35,9 @@ pub struct Socket {
     pub remote_addr: Ipv4Addr,
     pub local_port: u16,
     pub remote_port: u16,
+    pub send_param: SendParam,
+    pub recv_param: RecvParam,
+    pub status: TcpStatus,
     pub sender: TransportSender,
 }
 
@@ -45,6 +48,7 @@ impl Socket {
         remote_addr: Ipv4Addr,
         local_port: u16,
         remote_port: u16,
+        status: TcpStatus,
     ) -> Result<Self> {
         let (sender, _) = transport::transport_channel(65535, TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)),)?;
         return Ok(Self {
@@ -52,16 +56,45 @@ impl Socket {
             remote_addr,
             local_port,
             remote_port,
+            status: status,
+            send_param: SendParam {
+                unacked_seq: 0,
+                initial_seq: 0,
+                next: 0,
+                window: SOCKET_BUFFER_SIZE as u16,
+            },
+            recv_param: RecvParam {
+                initial_seq: 0,
+                next: 0,
+                window: SOCKET_BUFFER_SIZE as u16,
+                tail: 0,
+            },
             sender
         });
     }
 
-    pub fn send_tcp_packet(&mut self, flag: u8, payload: &[u8]) -> Result<usize> {
+    pub fn send_tcp_packet(&mut self, seq: u32, ack: u32, flag: u8, payload: &[u8]) -> Result<usize> {
         let mut tcp_packet = TCPPacket::new(payload.len());
         tcp_packet.set_src(self.local_port);
         tcp_packet.set_dest(self.remote_port);
+        tcp_packet.set_seq(seq);
+        tcp_packet.set_ack(ack);
+
+        // オプションフィールドは使わないので固定（だそうです）
+        tcp_packet.set_data_offset(5);
         tcp_packet.set_flag(flag);
-        let sent_size = self.sender.send_to(tcp_packet.clone(), IpAddr::V4(self.remote_addr)).unwrap();
+        tcp_packet.set_window_size(self.recv_param.window);
+        tcp_packet.set_checksum(util::ipv4_checksum(
+            &tcp_packet.packet(),
+            8,
+            &[],
+            &self.local_addr,
+            &self.remote_addr,
+            IpNextHeaderProtocols::Tcp,
+        ));
+        let sent_size = self.sender.send_to(tcp_packet.clone(), IpAddr::V4(self.remote_addr))
+            .context(format!("failed to send: \n{:?}", tcp_packet))?;
+        dbg!("sent", &tcp_packet);
         return Ok(sent_size);
     }
 
