bdb54cd74f0b513884c20de56039bfffc15b5306
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-01 08:03:07 +0000

    [change]

--- src/tcp.rs
+++ src/tcp.rs
@@ -20,19 +20,34 @@ const MSS: usize = 1460;
 const PORT_RANGE: Range<u16> = 40000..60000;
 
 pub struct TCP {
-    sockets: HashMap<SocketID, Socket>,
+    sockets: RwLock<HashMap<SocketID, Socket>>,
+    event_condvar: (Mutex<Option<TCPEvent>>, Condvar)
 }
 
 impl TCP {
-    pub fn new() -> Self {
-        let sockets = HashMap::new();
 
-        //let tcp = Self { sockets: sockets };
-        Self { sockets }
+    pub fn new() -> Arc<Self> {
+        let sockets = RwLock::new(HashMap::new());
+        let tcp = Arc::new(Self{
+            sockets,
+            event_condvar: (Mutex::new(None), Condvar::new())
+        });
+        let cloned_tcp = tcp.clone();
+        std::thread::spawn(move || {
+            cloned_tcp.receive_handler().unwrap();
+        });
+        return tcp;
     }
 
-    fn select_unused_port(&self, _: &mut ThreadRng) -> Result<u16> {
-        Ok(33445)
+    fn select_unused_port(&self, rng: &mut ThreadRng) -> Result<u16> {
+        for _ in 0..(PORT_RANGE.end - PORT_RANGE.start) {
+            let local_port = rng.gen_range(PORT_RANGE);
+            let table = self.sockets.read().unwrap();
+            if table.keys().all(|k| local_port != k.2) {
+                return Ok(local_port);
+            }
+        }
+        anyhow::bail!("no avaiable port found.");
     }
 
     pub fn connect(&self, addr: Ipv4Addr, port: u16) -> Result<SocketID> {
@@ -42,16 +57,52 @@ impl TCP {
             addr,
             self.select_unused_port(&mut rng)?,
             port,
+            TcpStatus::SynSent
         )?;
-        socket.send_tcp_packet(tcpflags::SYN, &[])?;
+
+        socket.send_param.initial_seq = rng.gen_range(1..1 << 31);
+        socket.send_tcp_packet(socket.send_param.initial_seq, 0, tcpflags::SYN, &[])?;
+        socket.send_param.unacked_seq = socket.send_param.initial_seq;
+        socket.send_param.next = socket.send_param.initial_seq + 1;
+        let mut table =self.sockets.write().unwrap();
         let socket_id = socket.get_socket_id();
+
+        // ロックを解除してイベントの待機。
+        // 受信スレッドがロックを獲得できるようにするため。
+        drop(table);
+        self.write_event(socket_id, TCPEventKind::ConnectionComplete);
         return Ok(socket_id);
     } 
 
+    fn recieve_handler(&self) -> Result<()> {
+        dbg!("begin recv thread");
+        let (_, mut receiver) = transport::transport_channel(
+            65535,
+            TransportChannelType::Layer3(IpNextHeaderProtocols::Tcp),
+        )?;
+        let mut packet_iter = transport::ipv4_packet_iter(&mut receiver);
+
+        Ok();
+    }
     
 }
 
 
 fn get_source_addr_to(addr: Ipv4Addr) -> Result<Ipv4Addr> {
-    return Ok("10.0.0.1".parse().unwrap());
+    //return Ok("10.0.0.1".parse().unwrap());
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!("ip route get {} | grep src", addr))
+        .output()?;
+    let mut output = str::from_utf8(&output.stdout)?
+        .trim()
+        .split_ascii_whitespace();
+        while let Some(s) = output.next() {
+            if s == "src" {
+                break;
+            }
+        }
+        let ip = output.next().context("failed to get src ip")?;
+        dbg!("source addr", ip);
+        return ip.parse().context("failed to parse source ip");
 }
